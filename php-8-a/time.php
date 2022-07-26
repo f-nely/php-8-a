086612611d43529bcbdcc9b568c529495121cae4
nely de faria <nelly.nf54@gmail.com>
2022-07-26 17:36:23 -0300

    Cria arquivo time.php

---
+++ php-8-a/time.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+UNIX TIMESTAMP
+O unix timestamp é o total de segundos decorridos desde 1 de janeiro de 1970, 
+que é a data que marca o início da era do sistema Unix.
+*/
+
+// echo time();
+
+// valor em microssegundos
+echo microtime(true);

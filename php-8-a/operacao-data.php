49ca167463eead08a1fa46c94e172d0bb6c558be
nely de faria <nelly.nf54@gmail.com>
2022-08-05 18:52:19 -0300

    Fazendo novas operações

--- php-8-a/operacao-data.php
+++ php-8-a/operacao-data.php
@@ -20,3 +20,17 @@ echo '<br>';
 $interval = new DateInterval('P180D');
 $date->add($interval);
 echo $date->format('M d, Y H:i:s'); // Jun 13, 2022 00:00:00
+echo '<br>';
+echo '<hr>';
+
+$data = new DateTime('01-01-2000 00:00:00');
+echo $data->format('d-m-Y H:i:s'); // 01-01-2000 00:00:00
+echo '<br>';
+
+$data->add(new DateInterval('P30D'));
+echo $data->format('d-m-Y H:i:s'); // 31-01-2000 00:00:00
+echo '<br>';
+
+$data->sub(new DateInterval('P20D'));
+echo $data->format('d-m-Y H:i:s'); // 11-01-2000 00:00:00
+echo '<br>';

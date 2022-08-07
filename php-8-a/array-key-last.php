445213a6e6e20eca5a9c2731aca5b952a9b5be4a
nely de faria <nelly.nf54@gmail.com>
2022-08-07 11:53:22 -0300

    Cria array-key-last.php

---
+++ php-8-a/array-key-last.php
@@ -0,0 +1,11 @@
+<?php
+
+// ARRAYS - PRIMEIRA E ÚLTIMA CHAVE DE UM ARRAY
+
+$empresa = [
+    'administrador' => 'joão',
+    'secretária' => 'ana',
+    'contabilista' => 'carlos',
+];
+
+echo array_key_last($empresa);

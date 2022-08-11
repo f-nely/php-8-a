09ae83e238339912867242b3b162c988262cf5df
nely de faria <nelly.nf54@gmail.com>
2022-08-11 17:01:21 -0300

    Cria form.php

---
+++ php-8-a/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="tratar.php" method="POST">
+        <p><input type="text" name="user" id="user" placeholder="Type your name"></p>
+        <p><input type="password" name="password" id="password" placeholder="Type your password"></p>
+        <p><input type="submit" value="Login"></p>
+    </form>
+</body>
+</html>

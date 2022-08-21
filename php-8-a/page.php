c790837a3e6ad7c932156523c259a10eb35a32ac
nely de faria <nelly.nf54@gmail.com>
2022-08-21 18:39:07 -0300

    Cria page.php

---
+++ php-8-a/page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page</title>
+</head>
+<body>
+    <p>Captação de valores de um formulário em PHP</p>
+    <hr>
+    <ul>
+        <li><a href="checkboxes.php" target="_blank">Checkboxes</a></li>
+        <li><a href="radiobuttons.php" target="_blank">Radio buttons</a></li>
+    </ul>
+</body>
+</html>

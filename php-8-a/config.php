3110d65815649d1453d6be8f2e0647da9fa3a447
nely de faria <nelly.nf54@gmail.com>
2022-08-27 19:46:49 -0300

    Cria config.php

---
+++ php-8-a/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('MYSQL_HOST',              'mysql');
+define('MYSQL_DATABASE',          'shop');
+define('MYSQL_USER',              'root');
+define('MYSQL_PASS',              '<root-password>');

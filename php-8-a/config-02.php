0efd53d7a7231659308931c7fe9c76a8178b030a
nely de faria <nelly.nf54@gmail.com>
2022-09-09 19:41:26 -0300

    Cria config-02.php

---
+++ php-8-a/config-02.php
@@ -0,0 +1,17 @@
+<?php
+
+define('MYSQL_1', [
+    'db_host' => 'mysql',
+    'db_name' => 'teste',
+    'db_user' => 'root',
+    'db_pass' => '<root-password>',
+    'db_char' => 'utf8',
+]);
+
+define('MYSQL_2', [
+    'db_host' => 'mysql',
+    'db_name' => 'shop',
+    'db_user' => 'root',
+    'db_pass' => '<root-password>',
+    'db_char' => 'utf8',
+]);

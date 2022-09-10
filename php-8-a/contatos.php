eb6d17bdc3741c91e0f47de7166f7d06f7a8f51d
nely de faria <nelly.nf54@gmail.com>
2022-09-10 20:17:14 -0300

    Cria contatos.php

---
+++ php-8-a/contatos.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contatos</title>
+</head>
+<body>
+    <h3>Contatos</h3>
+    <hr>
+    <a href="adicionar-contatos.php">Adicionar contatos</a>
+    <span> | </span>
+    <a href="mostrar-contatos.php">Mostrar contatos</a>
+</body>
+</html>
